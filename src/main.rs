ed9fcb702b5c4d166fb9326ad5d521b39e9037ca
Micah Wylde <micah@micahw.com>
2019-04-14 21:01:22 -0700

    hey basic looping working

---
+++ src/main.rs
@@ -0,0 +1,229 @@
+extern crate jack;
+extern crate crossbeam;
+use std::io;
+use crossbeam::queue::SegQueue;
+use std::f32::NEG_INFINITY;
+
+fn main() {
+    // Create client
+    let (client, _status) =
+        jack::Client::new("loopers", jack::ClientOptions::NO_START_SERVER).unwrap();
+
+    // Register ports. They will be used in a callback that will be
+    // called when new data is available.
+    let in_a = client
+        .register_port("rust_in_l", jack::AudioIn::default()).unwrap();
+    let in_b = client
+        .register_port("rust_in_r", jack::AudioIn::default()).unwrap();
+    let mut out_a = client
+        .register_port("rust_out_l", jack::AudioOut::default()).unwrap();
+    let mut out_b = client
+        .register_port("rust_out_r", jack::AudioOut::default()).unwrap();
+
+    let controller = client
+        .register_port("rust_midi_in", jack::MidiIn::default()).unwrap();
+
+    let mut buffer: Vec<[Vec<f32>; 2]> = vec![];
+
+    #[derive(Ord, PartialOrd, PartialEq, Eq, Debug)]
+    enum RecordMode {
+        NONE, READY, RECORD, OVERDUB
+    }
+
+    #[derive(Ord, PartialOrd, PartialEq, Eq, Debug)]
+    enum PlayMode {
+        PAUSED, PLAYING
+    }
+
+    let mut pos = 0usize;
+
+    const THRESHOLD: f32 = 0.1;
+
+    let mut record_mode = RecordMode::NONE;
+    let mut play_mode = PlayMode::PAUSED;
+
+    fn max_abs(b: &[f32]) -> f32 {
+        b.iter().map(|v| v.abs())
+            .fold(NEG_INFINITY, |a, b| a.max(b))
+    }
+
+    let process_callback = move |_: &jack::Client, ps: &jack::ProcessScope| -> jack::Control {
+        let out_a_p = out_a.as_mut_slice(ps);
+        let out_b_p = out_b.as_mut_slice(ps);
+        let in_a_p = in_a.as_slice(ps);
+        let in_b_p = in_b.as_slice(ps);
+
+        controller.iter(ps).for_each(|e| {
+            println!("{:?}", e);
+            if e.bytes.len() == 3 && e.bytes[0] == 144 {
+                match e.bytes[1]{
+                    60 => {
+                        if buffer.is_empty() || play_mode == PlayMode::PAUSED {
+                            record_mode = RecordMode::READY;
+                        } else {
+                            record_mode = RecordMode::OVERDUB;
+                        }
+                    }
+                    62 => {
+                        record_mode = RecordMode::NONE;
+                        play_mode = PlayMode::PLAYING;
+                        pos = 0;
+                    },
+                    64 => {
+                        play_mode = PlayMode::PAUSED;
+                    }
+                    _ => {}
+                }
+                println!("r = {:?}; p ={:?}", record_mode, play_mode);
+            }
+        });
+
+        if record_mode == RecordMode::READY && (max_abs(in_a_p) > THRESHOLD || max_abs(in_b_p) > THRESHOLD) {
+            buffer = vec![];
+            record_mode = RecordMode::RECORD;
+            println!("recording");
+        }
+
+        let mut l = in_a_p.to_vec();
+        let mut r = in_b_p.to_vec();
+
+        if play_mode == PlayMode::PLAYING {
+            if !buffer.is_empty() {
+                let len = buffer.len();
+                let el = &mut buffer[pos as usize % len];
+                for i in 0..el[0].len() {
+                    l[i] += el[0][i];
+                    r[i] += el[1][i];
+
+                    if record_mode == RecordMode::OVERDUB {
+                        el[0][i] += in_a_p[i];
+                        el[1][i] += in_b_p[i];
+                    }
+                }
+
+                pos += 1;
+            }
+        }
+
+        out_a_p.clone_from_slice(&l);
+        out_b_p.clone_from_slice(&r);
+
+        if record_mode == RecordMode::RECORD {
+            buffer.push([l, r]);
+        }
+
+        jack::Control::Continue
+    };
+    let process = jack::ClosureProcessHandler::new(process_callback);
+
+    // Activate the client, which starts the processing.
+    let active_client = client.activate_async(Notifications, process).unwrap();
+
+    // Wait for user input to quit
+    println!("Press enter/return to quit...");
+    let mut user_input = String::new();
+    io::stdin().read_line(&mut user_input).ok();
+
+    active_client.deactivate().unwrap();
+}
+
+struct Notifications;
+
+impl jack::NotificationHandler for Notifications {
+    fn thread_init(&self, _: &jack::Client) {
+        println!("JACK: thread init");
+    }
+
+    fn shutdown(&mut self, status: jack::ClientStatus, reason: &str) {
+        println!(
+            "JACK: shutdown with status {:?} because \"{}\"",
+            status, reason
+        );
+    }
+
+    fn freewheel(&mut self, _: &jack::Client, is_enabled: bool) {
+        println!(
+            "JACK: freewheel mode is {}",
+            if is_enabled { "on" } else { "off" }
+        );
+    }
+
+    fn buffer_size(&mut self, _: &jack::Client, sz: jack::Frames) -> jack::Control {
+        println!("JACK: buffer size changed to {}", sz);
+        jack::Control::Continue
+    }
+
+    fn sample_rate(&mut self, _: &jack::Client, srate: jack::Frames) -> jack::Control {
+        println!("JACK: sample rate changed to {}", srate);
+        jack::Control::Continue
+    }
+
+    fn client_registration(&mut self, _: &jack::Client, name: &str, is_reg: bool) {
+        println!(
+            "JACK: {} client with name \"{}\"",
+            if is_reg { "registered" } else { "unregistered" },
+            name
+        );
+    }
+
+    fn port_registration(&mut self, _: &jack::Client, port_id: jack::PortId, is_reg: bool) {
+        println!(
+            "JACK: {} port with id {}",
+            if is_reg { "registered" } else { "unregistered" },
+            port_id
+        );
+    }
+
+    fn port_rename(
+        &mut self,
+        _: &jack::Client,
+        port_id: jack::PortId,
+        old_name: &str,
+        new_name: &str,
+    ) -> jack::Control {
+        println!(
+            "JACK: port with id {} renamed from {} to {}",
+            port_id, old_name, new_name
+        );
+        jack::Control::Continue
+    }
+
+    fn ports_connected(
+        &mut self,
+        _: &jack::Client,
+        port_id_a: jack::PortId,
+        port_id_b: jack::PortId,
+        are_connected: bool,
+    ) {
+        println!(
+            "JACK: ports with id {} and {} are {}",
+            port_id_a,
+            port_id_b,
+            if are_connected {
+                "connected"
+            } else {
+                "disconnected"
+            }
+        );
+    }
+
+    fn graph_reorder(&mut self, _: &jack::Client) -> jack::Control {
+        println!("JACK: graph reordered");
+        jack::Control::Continue
+    }
+
+    fn xrun(&mut self, _: &jack::Client) -> jack::Control {
+        println!("JACK: xrun occurred");
+        jack::Control::Continue
+    }
+
+    fn latency(&mut self, _: &jack::Client, mode: jack::LatencyType) {
+        println!(
+            "JACK: {} latency has changed",
+            match mode {
+                jack::LatencyType::Capture => "capture",
+                jack::LatencyType::Playback => "playback",
+            }
+        );
+    }
+}
